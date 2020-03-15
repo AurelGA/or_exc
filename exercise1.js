496f6c7e998c2a4ecd7097a5dcc456197c32bcb1
Aurel Zhen Xing - The Immortal Engineer <animasupreme@Aurels-MacBook-Pro.local>
2020-03-15 14:26:55 -0700

    exercise1 created and coded.

---
+++ exercise1.js
@@ -0,0 +1,13 @@
+var name = "Aurel";
+
+var age = 30;
+
+var address = "6535 Vulcan Way NW";
+
+var greeting = 'Hello, my name is ' + name + ' ' + '. I am ' + age + ' and I live at ' + address; 
+
+console.log(greeting);
+
+var greeting2 = `Hello, my name is  ${name},  I am ${age}, and I live at ${address}.`
+ 
+console.log(greeting2);
